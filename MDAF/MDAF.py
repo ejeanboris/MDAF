4f614b958ed3cd69a51d8fbd6073f464cadf9954
Remi Ehounou <ejeanboris@live.com>
2021-05-24 00:55:24 -0400

    debug

--- MDAF/MDAF.py
+++ MDAF/MDAF.py
@@ -95,11 +95,11 @@ def measure(heuristicpath, funcpath, args, connection):
     upper = funcChars['upper']
     lower = funcChars['lower']
 
-    if upper is not list: upper = [upper for i in range(n)]
+    if upper is not list: 
+        print(type(upper) 
+        upper = [upper for i in range(n)]
     if lower is not list: lower = [lower for i in range(n)]
 
-    print(upper)
-    print(lower)
     scale = list()
     for i in range(n): 
         scale.append(upper[i] - lower[i])
